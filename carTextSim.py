5cae5d4c475d71d78db452d55195fa0e313d96f6
Vnc <76777885+VinceValmeo@users.noreply.github.com>
2021-07-14 19:57:44 +0800

    Add files via upload

---
+++ carTextSim.py
@@ -0,0 +1,27 @@
+
+started = False
+while True:
+    command = input(">").lower()
+    
+    if command == "start":
+        if started:
+            print("Car already started!")
+        else:
+            started = True
+            print("Car started...Ready to go!")
+
+    elif command == "stop":
+        if not started:
+            print("Car already stopped.")
+        else:
+            started = False
+            print("Car stopped")
+    
+    elif command == "help":
+        print("start - to start the car")
+        print("stop - to stop the car")
+        print("quit - to exit")
+    elif command == "quit": 
+        break
+    else: 
+        print("I don't understand that...")
